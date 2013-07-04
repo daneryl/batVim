1b805d485b9e9aafd9b8dcd608eeeeea69edc244
daneryl <daneryl@gmail.com>
2013-07-04 15:48:11 -0500

    installer improved

--- batinstall.sh
+++ batinstall.sh
@@ -1,7 +1,14 @@
-rm -fR ~/.vim
-mkdir ~/.vim
-cp -R ./vim/* ~/.vim
-cat ./vimrc > ~/.vimrc
+#rm -fR ~/.vim
+#mkdir ~/.vim
+#cp -R ./vim/* ~/.vim
+#cat ./vimrc > ~/.vimrc
+
+cp -fR $(pwd)/* ~/.bat_vim 2>/dev/null;
+
+echo 'set runtimepath+=~/.bat_vim
+source ~/.bat_vim/config/base.vim
+source ~/.bat_vim/config/plugins.vim
+source ~/.bat_vim/config/binds.vim' > ~/.vimrc
 
 echo -e "\033[0;33m"
 cat ./success.txt 
