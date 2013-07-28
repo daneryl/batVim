c0ababe1918a52f3b56aaf92efdde0016771d977
daneryl <daneryl@gmail.com>
2013-07-27 20:28:39 -0500

    fixed batinstall.sh

--- batinstall.sh
+++ batinstall.sh
@@ -1,15 +1,11 @@
-#rm -fR ~/.vim
-#mkdir ~/.vim
-#cp -R ./vim/* ~/.vim
-#cat ./vimrc > ~/.vimrc
-
 mkdir ~/.bat_vim 2>/dev/null
 cp -fR $(pwd)/* ~/.bat_vim 2>/dev/null
 
 echo 'set runtimepath+=~/.bat_vim
 source ~/.bat_vim/config/base.vim
 source ~/.bat_vim/config/plugins.vim
-source ~/.bat_vim/config/binds.vim' > ~/.vimrc
+source ~/.bat_vim/config/binds.vim
+source ~/.bat_vim/config/unite.vim' > ~/.vimrc
 
 echo "\033[0;33m"
 cat ./success.txt 
