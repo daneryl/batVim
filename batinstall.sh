5e88834c7d283e0eb28a6e047c8d86f8ccd9d1aa
daneryl <daneryl@gmail.com>
2014-02-27 21:48:18 -0500

    batinstaller with correct yellow batman color

--- batinstall.sh
+++ batinstall.sh
@@ -14,10 +14,10 @@ source ~/.bat_vim/config/unite.vim' > ~/.vimrc
 
 if [ ! -w ~/.viminfo ] 
 then 
-echo "\033[31myou need write permission for .viminfo file\n\033[0m"
+echo -e "\033[31myou need write permission for .viminfo file\n\033[0m"
 fi
 make -C ~/.bat_vim/bundle/vimproc.vim/
 
-echo "\033[0;33m"
+echo -e "\033[0;33m"
 cat ./success.txt 
-echo "\033[0m"
+echo -e "\033[0m"
